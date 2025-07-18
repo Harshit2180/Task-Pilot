d1c15443176902fcecc1a5ec96de75b27dda3fd9
Harshit2180 <chopraharshit2180@gmail.com>
2025-07-18 20:28:19 +0530

    Desgin login and signup page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from 'react'
-
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import Signup from './pages/Auth/Signup'
+import MainLayout from './components/layouts/MainLayout'
 
 function App() {
-  
+
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "login",
+          element: <Login />
+        },
+        {
+          path: "signup",
+          element: <Signup />
+        }
+      ]
+    }
+  ])
+
   return (
     <>
-      <div className='text-blue-700'>Hello</div>
+      <RouterProvider router={appRouter} />
     </>
   )
 }
---
+++ Frontend/src/components/Inputs/ProfilePhotoSelector.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react';
+import { useRef } from 'react'
+import { LuUser, LuUpload, LuTrash } from 'react-icons/lu'
+
+const ProfilePhotoSelector = ({ image, setImage }) => {
+
+    const inputRef = useRef(null);
+    const [previewUrl, setPreviewUrl] = useState(null);
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            setImage(file);
+            const preview = URL.createObjectURL(file);
+            setPreviewUrl(preview)
+        }
+    }
+
+    const handleRemoveImage = () => {
+        setImage(null);
+        setPreviewUrl(preview)
+    }
+
+    const onChooseFile = () => {
+        inputRef.current.click();
+    }
+
+    return (
+        <div className='flex justify-center mb-6'>
+            <input type="file" accept="image/*" ref={inputRef} onChange={handleImageChange} className='hidden' />
+            {
+                !image ? (
+                    <div className='w-20 h204 flex items-center justify-center bg-blue-100/50 rounded-full relative cursor-pointer'>
+                        <LuUser className='text-4xl text-[#1368EC]' />
+                        <button type='button' className='w-8 h-8 flex items-center bg-[#1368ec] justify-center text-white rounded-full absolute -bottom-1 -right-t cursor-pointer' onClick={onChooseFile}><LuUpload /></button>
+                    </div>
+                ) : (
+                    <div className='relative'>
+                        <img src={previewUrl} alt="profile photo" className='w-20 h-20 roundedful object-cover' />
+                        <button type='button' className='w-8 h-8 flex items-center justify-center bg-red-500 text-white rounded-full absolute -bottom-1 -right-1' onClick={handleRemoveImage}><LuTrash /></button>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default ProfilePhotoSelector
