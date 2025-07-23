5b83067331c0a31956e1143ac71ea8c115ae0b1f
Harshit2180 <chopraharshit2180@gmail.com>
2025-07-23 17:55:12 +0530

    Create charts

---
+++ Frontend/src/pages/Admin/CreateTask.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import DashboardLayout from '../../components/layouts/DashboardLayout'
+import { PRIORITY_DATA } from '../../utils/data'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPaths'
+import toast from 'react-hot-toast '
+import { useLocation, useNavigate } from 'react-router-dom'
+import moment from 'moment'
+import { LuTrash2 } from 'react-icons/lu'
+
+
+const CreateTask = () => {
+
+    const location = useLocation();
+    const { taskId } = location.state || {}
+    const navigate = useNavigate()
+
+    const [taskData, setTaskData] = useState({
+        title: "",
+        description: "",
+        priority: "Low",
+        dueDate: null,
+        assignedTo: [],
+        todoChecklist: [],
+        attachments: []
+    })
+
+    const [currentTask, setCurrentTask] = useState(null)
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [openDeleteAlert, setOpenDeleteAlert] = useState(false)
+
+    const handleValueChange = (key, value) => {
+        setTaskData((prevData) => ({ ...prevData, [key]: value }))
+    }
+
+    const clearData = () => {
+        setTaskData({
+            title: "",
+            description: "",
+            priority: "Low",
+            dueDate: null,
+            assignedTo: [],
+            todoChecklist: [],
+            attachments: []
+        })
+    }
+
+    return (
+        <DashboardLayout activeMenu="Create Task">Create Task</DashboardLayout>
+    )
+}
+
+export default CreateTask
--- Frontend/src/pages/Admin/Dashboard.jsx
+++ Frontend/src/pages/Admin/Dashboard.jsx
@@ -10,6 +10,10 @@ import { addThousandsSeparator } from '../../utils/helper';
 import InfoCard from '../../components/Cards/InfoCard';
 import { LuArrowRight } from 'react-icons/lu';
 import TaskListTable from '../../components/TaskListTable';
+import CustomPieChart from '../../components/Charts/CustomPieChart';
+import CustomBarChart from '../../components/Charts/CustomBarChart';
+
+const COLORS = ["#8D51FF", "#00B8DB", "#7BCE00"]
 
 const Dashboard = () => {
 
@@ -21,6 +25,28 @@ const Dashboard = () => {
     const [pieChartData, setPieChartData] = useState([]);
     const [barChartData, setBarChartData] = useState([]);
 
+    const prepareChartData = (data) => {
+        const taskDistribution = data?.taskDistribution || null;
+        const taskPriorityLevels = data?.taskPriorityLevels || null;
+
+
+        const taskDistributionData = [
+            { status: "Pending", count: taskDistribution?.Pending || 0 },
+            { status: "In Progress", count: taskDistribution?.InProgress || 0 },
+            { status: "Completed", count: taskDistribution?.Completed || 0 }
+        ]
+
+        setPieChartData(taskDistributionData);
+
+        const PriorityLevelsData = [
+            { priority: "Low", count: taskPriorityLevels?.Low || 0 },
+            { priority: "Medium", count: taskPriorityLevels?.Medium || 0 },
+            { priority: "High", count: taskPriorityLevels?.High || 0 }
+        ]
+
+        setBarChartData(PriorityLevelsData);
+    }
+
     const getDashboardData = async () => {
         try {
 
@@ -30,6 +56,7 @@ const Dashboard = () => {
 
             if (response.data) {
                 setDashboardData(response.data)
+                prepareChartData(response.data?.charts || null)
             }
 
         } catch (error) {
@@ -64,6 +91,22 @@ const Dashboard = () => {
                 </div>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6'>
+                <div>
+                    <div className='card'>
+                        <div className='flex items-center justify-center'>
+                            <h5 className='font-medium'>Task Distribution</h5>
+                        </div>
+                        <CustomPieChart data={pieChartData} colors={COLORS} />
+                    </div>
+                </div>
+                <div>
+                    <div className='card'>
+                        <div className='flex items-center justify-center'>
+                            <h5 className='font-medium'>Task Priority Levels</h5>
+                        </div>
+                        <CustomBarChart data={barChartData} />
+                    </div>
+                </div>
                 <div className='md:col-span-2'>
                     <div className='card'>
                         <div className='flex items-center justify-between'>
